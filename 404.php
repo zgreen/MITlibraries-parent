549218687289d02809b87fbfb1a95f9ae08afba2
Zach Green <zgreen@mit.edu>
2014-04-04 16:14:12 -0400

    Fix 404 search form height

--- 404.php
+++ 404.php
@@ -22,7 +22,7 @@ get_header(); ?>
 			<div class="entry-content">
 				<h2><?php _e('Search the Libraries\' web site:', 'twentytwelve' ); ?></h2>
 
-				<form action="http://www.google.com/cse" id="cse-search-box">
+				<form action="http://www.google.com/cse" id="cse-search-box" class="search">
 				  <div>
 				    <input type="hidden" name="cx" value="012139403769412284441:qmnizspyywg">
 				    <input type="hidden" name="ie" value="UTF-8">
