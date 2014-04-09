3b7b40b5e19880f905e7099712aebec77633c3f6
Zach Green <zgreen@mit.edu>
2014-04-09 15:11:12 -0400

    Add site-wide maintenance page

--- header.php
+++ header.php
@@ -1,4 +1,10 @@
 <?php
+
+if (!is_user_logged_in()) {
+	get_template_part('inc/maintenance', 'site');
+	exit;
+}
+
 /**
  * The Header for our theme.
  *
---
+++ inc/maintenance-site.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Testing</title>
+</head>
+<body>
+	<h1>Briefly unavailable for scheduled maintenance. Check back in a minute.</h1>
+</body>
+</html>
