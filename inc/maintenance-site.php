e79e72b4c7cd83577b7e580569098c8d4d999e26
Zach Green <zgreen@mit.edu>
2014-04-09 15:13:24 -0400

    Add body class maintenance

--- inc/maintenance-site.php
+++ inc/maintenance-site.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<title>Testing</title>
 </head>
-<body>
+<body class="maintenance">
 	<h1>Briefly unavailable for scheduled maintenance. Check back in a minute.</h1>
 </body>
 </html>
