fa79190d705e96715f62868cbf0812d7cf63c1b0
Zach Green <zgreen@mit.edu>
2014-04-09 15:17:42 -0400

    Use Open Sans on maintenance page

--- inc/maintenance-site.php
+++ inc/maintenance-site.php
@@ -3,6 +3,13 @@
 <head>
 	<meta charset="UTF-8">
 	<title>libraries.mit.edu Under Maintenance</title>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
+	<style>
+		.maintenance {
+			font-family: 'Open Sans', sans-serif;
+			font-weight: 300;
+		}
+	</style>
 </head>
 <body class="maintenance">
 	<h1>Briefly unavailable for scheduled maintenance. Check back in a minute.</h1>
