e5be7e67bee442759dd7a872e3b32f5de8ee2304
Zach Green <zgreen@mit.edu>
2014-04-09 15:24:18 -0400

    Add maintenance page bg-image

--- inc/maintenance-site.php
+++ inc/maintenance-site.php
@@ -5,6 +5,13 @@
 	<title>libraries.mit.edu Under Maintenance</title>
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
 	<style>
+		html {
+			background: url(/wp-content/themes/libraries/images/dome-indoor-blurred-sfw.jpg) no-repeat center center fixed; 
+		  -webkit-background-size: cover;
+		  -moz-background-size: cover;
+		  -o-background-size: cover;
+		  background-size: cover;
+		}
 		.maintenance {
 			font-family: 'Open Sans', sans-serif;
 			font-weight: 300;
