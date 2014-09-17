87604bcfcd1b4078be0f87fd33f37738154c5f86
Zach Green <zgreen@mit.edu>
2014-09-17 12:17:11 -0400

    Revert d6bfa8c..f7b547a
    This rolls back to commit d6bfa8c3d5e5d1e4a5c6eebcb51984ad978cec1e.

--- js/search.js
+++ js/search.js
@@ -3,6 +3,119 @@
 //
 
 $(function(){
+	
+	// Mimic a <select> element with a <ul>
+	$('#resources').on('click', 'li', function(event) {
+		// All available resources	
+		var resourcesAll = $('#resources li');
+		// Cancel if the li has a link in it
+		if ($(this).hasClass('has-link')) {
+			return;
+		}
+		else {
+			// To show or hide the parent <ul>
+			$(this).parent().toggleClass('active');
+			// Remove active class from any <li> that has it...
+			$(resourcesAll).removeClass('active');
+			// And add the class to the <li> that gets clicked
+			$(this).toggleClass('active');
+			
+			// Get the main text of the currently selected <li>
+			var selectedText = $('#resources li.active .main').text();
+			// Show this text above the dropdown (when active), along with select arrows SVG, mimicing a <select>
+			if ($('#resources').hasClass('active')) {
+				// Open
+				$('.wrap-select--resources .selected').text(selectedText).append('<svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="8.071px" height="14px" viewBox="0 0 8.071 14" enable-background="new 0 0 8.071 14" xml:space="preserve" class="select-arrows"><path d="M0.069 8.74c0.08-0.159 0.252-0.264 0.436-0.274 0.023 0 1.743-0.104 3.531-0.104s3.508 0.104 3.53 0.104C7.751 8.476 7.911 8.581 8.003 8.74c0.092 0.161 0.092 0.356 0 0.517 -1.364 2.431-3.508 4.517-3.6 4.598 -0.206 0.194-0.528 0.194-0.734 0 -0.091-0.081-2.235-2.167-3.6-4.598C-0.023 9.096-0.023 8.901 0.069 8.74M8.003 5.259c-0.08 0.16-0.252 0.264-0.437 0.275 -0.022 0-1.742 0.103-3.53 0.103S0.528 5.535 0.505 5.535C0.321 5.523 0.161 5.419 0.069 5.259c-0.092-0.161-0.092-0.355 0-0.516 1.365-2.431 3.508-4.517 3.6-4.598 0.206-0.194 0.528-0.194 0.734 0 0.092 0.081 2.235 2.167 3.6 4.598C8.095 4.904 8.095 5.099 8.003 5.259"></path></svg>').addClass('active');
+			}
+			// Remove the text when the dropdown is closed
+			else {
+				// Closed
+				$('.wrap-select--resources .selected').text('').removeClass('active');
+				// Remove any input text
+				$('#search-main input').val('');
+				$(this).trigger('search-change');
+			}
+			// // Get the class of the selected resource
+			// var searchSelected = $('#resources li.active').attr('data-target');
+			// // Apply this class, as an id, to the form.
+			// $('#search-main form').attr('id', searchSelected);
+		}
+	});
+	// Close the faux select menu when clicking outside it 
+	$(document).on('click', function(event){
+		if(!$('#resources.active').has(event.target).length == 0) {
+			return;
+		}
+		else {
+			$('#resources').removeClass('active');
+			$('#search-main .selected').removeClass('active').text('');
+		}
+	});
+	
+	// Placeholder text changes
+	function searchBy() {
+		var optionSelected = $('#search-main select.active option:selected').val();
+		if ($('#bartonplus.active').length) {
+			if(optionSelected == '') {
+				$('input.active').attr('placeholder', 'ex: carbon nanotubes');
+			}
+			if(optionSelected == 'TI ') {
+				$('input.active').attr('placeholder', 'ex: freakonomics');
+			}
+			if(optionSelected == 'AU ') {
+				$('input.active').attr('placeholder', 'ex: noam chomsky');
+			}
+		}
+		if ($('#vera.active').length) {
+			if(optionSelected == 'contains') {
+				$('input.active').attr('placeholder', 'ex: new eng j of med');
+			}
+			if(optionSelected == 'startsWith') {
+				$('input.active').attr('placeholder', 'ex: journal of cell biology');
+			}
+			if(optionSelected == 'exactMatch') {
+				$('input.active').attr('placeholder', 'ex: web of science');
+			}
+		}
+		if ($('#barton.active').length) {
+			if(optionSelected == 'find_WRD') {
+				$('input.active').attr('placeholder', 'ex: game design');
+			}
+			if(optionSelected == 'scan_TTL') {
+				$('input.active').attr('placeholder', 'ex: introduction to fluid mechanics');
+			}
+			if(optionSelected == 'scan_AUT') {
+				$('input.active').attr('placeholder', 'ex: shakespeare william');
+			}
+			if(optionSelected == 'scan_CND') {
+				$('input.active').attr('placeholder', 'ex: ta405.t5854');
+			}
+		}
+		if ($('#worldcat.active').length) {
+			if(optionSelected == 'keyword') {
+				$('input.active').attr('placeholder', 'ex: carbon nanotubes');
+			}
+			if(optionSelected == 'author') {
+				$('input.active').attr('placeholder', 'ex: william shakespeare');
+			}
+			if(optionSelected == 'title') {
+				$('input.active').attr('placeholder', 'ex: introduction to fluid mechanics');
+			}
+		}
+		if ($('#course-reserves.active').length) {
+			if(optionSelected == 'scan_CNB') {
+				$('input.active').attr('placeholder', 'ex: 21F.108');
+			}
+			if(optionSelected == 'find_WIN') {
+				$('input.active').attr('placeholder', 'ex: cohen');
+			}
+			if(optionSelected == 'find_WOU') {
+				$('input.active').attr('placeholder', 'ex: introduction chemistry');
+			}
+		}
+	}
+	// Run searchby
+	searchBy();
 
 	function searchBySwitch() {
 		// Get the value of the active "search-by" option
