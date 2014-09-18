2fc84429773b1090d6356f59e1e714340939fe3f
Zach Green <zgreen@mit.edu>
2014-09-18 12:13:30 -0400

    save active search limiter and form

--- js/search/search-save-active.js
+++ js/search/search-save-active.js
@@ -6,32 +6,49 @@ $(function(){
   var resources = $('#resources');
   // The first search tool available in the dropdown
   var resFirst = $('li:first', resources);
+  var searchLimiter = resFirst.data('option');
+  var searchTarget = resFirst.data('target');
+  // Set the active limiter
+  //$('#search-main select.'+searchLimiter).addClass('active');
   // Check for localstorage using Modernizr
   if (Modernizr.localstorage) {
     // localStorage var
     var activated = localStorage.getItem('activeElement');
     // Check if that var is null
     if (activated === null) {
+      console.log('nothing stored yet');
       // Add active class to first ul#resources option if not saved to localStorage
       resFirst.addClass('active');
+      // Set the active limiter
+      $('#search-main select.'+searchLimiter).addClass('active').parent().addClass('active');
+      // Set the active search form
+      $('#search-main form#'+searchTarget).addClass('input-submit active');
+      $('#search-main input.'+searchLimiter).addClass('active')
+      searchBy();
+      $('#search-main a.search-advanced.'+searchTarget).addClass('active');
       // Get the data-target attribute of the active element
       var activeTar = resFirst.attr('data-target');
       // Set that ID as the activeElement item
       localStorage.setItem('activeElement', activeTar);
       console.log(activeTar);
       console.log(localStorage);
+
     }
     else {
-
+      
       var target = 'li[data-target="'+activated+'"]';
-
-      function activateStoredElement() {
-
-        $('li', resources).removeClass('active');
-        $(target, resources).addClass('active');
-
-      }
-      activateStoredElement();
+      console.log(activated);
+      $(target, resources).addClass('active');
+      var searchLimiter = $(target).data('option');
+      // Activate the corresponding search limiter
+      $('#search-main select.'+searchLimiter).addClass('active').parent().addClass('active');
+      // Activate the corresponding form
+      $('#search-main form#'+activated).addClass('input-submit active');
+      $('#search-main input.'+searchLimiter).addClass('active');
+      $('#search-main a.search-advanced.'+activated).addClass('active');
+      console.log('#search-main form#'+activated);
+      searchBy();
+      
 
       // Store the data-target of the active element
       function storeTarget() {
@@ -48,6 +65,7 @@ $(function(){
   }
   // No localStorage
   else {
+    // Activate the first search tool
     resFirst.addClass('active');
   }
 });
